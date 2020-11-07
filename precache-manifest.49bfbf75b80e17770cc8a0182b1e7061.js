21cd7e36aa68ad74ddacc23af758bdb4bec6327a
UditDuhan <uditduhan23@gmail.com>
2020-11-07 14:41:31 +0530

    Updates

---
+++ precache-manifest.49bfbf75b80e17770cc8a0182b1e7061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fca27ff77b21680aeb7d6c217d910d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e6bf83399279f6fd8e4b",
+    "url": "/react-todo/static/css/main.07a1d3c9.chunk.css"
+  },
+  {
+    "revision": "6c1454b5034b5582847d",
+    "url": "/react-todo/static/js/2.104b3b45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.104b3b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bf83399279f6fd8e4b",
+    "url": "/react-todo/static/js/main.3b9bfc7a.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
